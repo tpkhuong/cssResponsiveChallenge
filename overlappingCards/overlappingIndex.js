7099f1b8b04ed535630fd494c3e9f2c3d7efb937
Toan <tpkhuong@gmail.com>
2021-02-19 06:38:03 -0500

    our js code to work with wordpress site. pause video using tab or esc key

--- overlappingCards/overlappingIndex.js
+++ overlappingCards/overlappingIndex.js
@@ -1,7 +1,9 @@
 document
   .querySelector(".embed-responsive")
   .addEventListener("keydown", function whatKeyWasPressed(event) {
+    console.log(event);
     /* key: "Escape" keyCode: 27 or code: "Escape"*/
+    /*key: "Tab" code: "Tab" keyCode: 9 */
     console.dir(event.target);
     var videoElement = document.querySelector(".embed-responsive-item");
 
@@ -18,13 +20,21 @@ document
       event.code == "Escape"
     ) {
       //   console.log("we are hitting the Escape key");
-      // document.querySelector("div.close")
+      //   var closeBtn = document.querySelector("div.close");
       //   let closeBtn = document.querySelector(".fa.fa-times");
-      //   closeBtn.setAttribute("tabindex", "0");
       //   console.dir(videoElement);
+      //   console.dir(closeBtn);
+      //   closeBtn.setAttribute("tabindex", "0");
+      //   closeBtn.focus();
       videoElement.pause();
       document.querySelector(".uabb-button.uabb-creative-button").focus();
       //   console.log(document.querySelector(":focus"));
+    }
+    if (event.key == "Tab" || event.code == "Tab" || event.keyCode == 9) {
+      videoElement.pause();
+
+      console.log(event);
+      console.log("we hit the tab key");
     }
   });
 
