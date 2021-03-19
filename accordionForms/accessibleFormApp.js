606a01624efaac784de6981a6ea03d1650e32c31
Toan <tpkhuong@gmail.com>
2021-03-19 06:23:40 -0400

    work on showing panel based on button clicked

--- accordionForms/accessibleFormApp.js
+++ accordionForms/accessibleFormApp.js
@@ -1,13 +1,51 @@
 function ourSelectors() {
   var wrapperElement = document.querySelector(".wrapper");
   var arrOfBtnElement = document.querySelectorAll(".toggle-btn");
+  var arrOfHeadingElements = document.querySelectorAll(".toggle-heading");
 
   return {
     wrapperElement,
     arrOfBtnElement,
+    arrOfHeadingElements,
   };
 }
 
-function addFocusAndExpand({ wrapperElement } = ourSelectors()) {
-  wrapperElement.addEventListener("keydown", function checkFocus(event) {});
+addFocusAndExpand();
+
+function addFocusAndExpand(
+  { arrOfBtnElement, wrapperElement, arrOfHeadingElements } = ourSelectors()
+) {
+  var convertToArrBtn = Array.from(arrOfBtnElement);
+  var convertToArrHeadings = Array.from(arrOfHeadingElements);
+
+  convertToArrHeadings.forEach(function initialState(eachHeading) {
+    alert("check if heading has aria-expanded = false");
+    console.log(eachHeading.attributes["aria-expanded"].value);
+    // if(eachHeading.attributes[])
+  });
+
+  wrapperElement.addEventListener("focusin", function attachClass(event) {
+    if (event.target.className.includes("toggle-btn")) {
+      this.classList.add("focus-ring");
+    } else {
+      this.classList.remove("focus-ring");
+    }
+  });
+
+  wrapperElement.addEventListener("click", function showPanel(event) {
+    console.log(event);
+  });
 }
+
+/*
+
+wrapperElement.addEventListener("focus", function checkFocus(event) {
+    console.log(event);
+
+    // if () {
+    //   this.classList.add("focus-ring");
+    // } else {
+    //   this.classList.remove("focus-ring");
+    // }
+  });
+*/
