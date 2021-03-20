664070854354f262599a2837eb07b6aaff541743
Toan <tpkhuong@gmail.com>
2021-03-20 06:40:59 -0400

    clicking on button works js

--- accordionForms/accessibleFormApp.js
+++ accordionForms/accessibleFormApp.js
@@ -10,30 +10,58 @@ function ourSelectors() {
   };
 }
 
-addFocusAndExpand();
+expandContentPanel();
+addFocus();
 
-function addFocusAndExpand(
+function expandContentPanel(
   { arrOfBtnElement, wrapperElement, arrOfHeadingElements } = ourSelectors()
 ) {
   var convertToArrBtn = Array.from(arrOfBtnElement);
   var convertToArrHeadings = Array.from(arrOfHeadingElements);
 
-  convertToArrHeadings.forEach(function initialState(eachHeading) {
-    alert("check if heading has aria-expanded = false");
-    console.log(eachHeading.attributes["aria-expanded"].value);
-    // if(eachHeading.attributes[])
-  });
+  //   wrapperElement.addEventListener("focusin", function attachClass(event) {
+  //     if (event.target.className.includes("toggle-btn")) {
+  //       this.classList.add("focus-ring");
+  //     } else {
+  //       this.classList.remove("focus-ring");
+  //     }
+  //   });
 
+  wrapperElement.addEventListener("click", function showPanel(event) {
+    if (event.target.className.includes("toggle-btn")) {
+      let grandParentClass = event.target.parentElement.parentElement.className;
+      convertToArrHeadings.forEach(function printClass(eachHeading) {
+        var headingParentClass = eachHeading.parentElement.className;
+        if (headingParentClass != grandParentClass) {
+          eachHeading.attributes["aria-expanded"].value = "false";
+        } else {
+          eachHeading.attributes["aria-expanded"].value = "true";
+        }
+      });
+    } else if (event.target.className.includes("toggle-heading")) {
+      let clickHeadingElementParent = event.target.parentElement.className;
+      let focusClickedHeadingBtn = event.target.firstElementChild;
+      focusClickedHeadingBtn.focus();
+      convertToArrHeadings.forEach(function printClass(eachHeading) {
+        var headingParentClass = eachHeading.parentElement.className;
+        console.log(headingParentClass);
+        if (headingParentClass != clickHeadingElementParent) {
+          eachHeading.attributes["aria-expanded"].value = "false";
+        } else {
+          eachHeading.attributes["aria-expanded"].value = "true";
+        }
+      });
+    }
+  });
+}
+
+function addFocus({ wrapperElement } = ourSelectors()) {
   wrapperElement.addEventListener("focusin", function attachClass(event) {
     if (event.target.className.includes("toggle-btn")) {
       this.classList.add("focus-ring");
     } else {
       this.classList.remove("focus-ring");
     }
-  });
-
-  wrapperElement.addEventListener("click", function showPanel(event) {
-    console.log(event);
   });
 }
 
